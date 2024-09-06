a86f35b8b8dd2a4a983bb577d459fa3aa6094abc
Pavla_Beránková <berankovap@hotmail.com>
2024-09-06 17:34:48 +0200

    Initial Commit

---
+++ JobSearchLog/JobSearchLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobSearchLogApp.swift
+//  JobSearchLog
+//
+//  Created by Pavla Beránková on 06.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct JobSearchLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
